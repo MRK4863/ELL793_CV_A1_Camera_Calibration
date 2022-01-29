e046ad591c6194822994994d0d508df1f3a1270a
MRK4863 <mrkmrk1999@gmail.com>
2022-01-29 21:22:40 +0530

    added sample images and python code
    sample images added
    python code to calibrate is added

---
+++ camera_calib_python.py
@@ -0,0 +1,55 @@
+import numpy as np
+import cv2 as cv
+import glob
+ 
+chessboard_size = (24,17)
+frameSize = (1440, 1080)
+
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_EPS, 30, 0.001)
+
+objp = np.zeros((chessboard_size[0] * chessboard_size[1], 3), np.float32)
+objp[:,:2] = np.mgrid[0:chessboard_size[0], 0:chessboard_size[1]].T.reshape(-1,2)
+
+
+objPoints = []
+imgPoints = []
+
+images = glob.glob("E:\GitHub\CV_Assignment_1\SAMPLE_IMAGES\*.png")
+for image in images:
+    print(image)
+    img = cv.imread(image)
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    #Find the chess board corners
+    ret, corners = cv.findChessboardCorners(img_gray, chessboard_size, None)
+    
+    if ret == True:
+        objPoints.append(objp)
+        corners2 = cv.cornerSubPix(img_gray, corners, (11, 11), (-1, -1), criteria)
+        imgPoints.append(corners)
+
+        cv.drawChessboardCorners(img, chessboard_size, corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(1000)
+
+    cv.destroyAllWindows()
+
+ret, cameraMatrix, dist, rvecs, tvecs = cv.calibrateCamera(objPoints, imgPoints, frameSize, frameSize, None, None)
+
+print("Camera calibrated: ", ret)
+print("\nCamera Matrix: \n", cameraMatrix)
+print("\nDistortion parameters: \n", dist)
+print("\nRotation parameters: \n", rvecs)
+print("\nTranslation parameters: \n", tvecs)
+
+#ERROR
+
+mean_error = 0
+
+for i in range(len(objPoints)):
+    imgPoints2, _ = cv.projectPoints(objPoints[1], rvecs[i], tvecs[i], cameraMatrix, dist)
+    error = cv.norm(imgPoints[i], imgPoints2, cv.NORM_L2)/len(imgPoints2)
+    mean_error +=error
+    
+print("\ntotal error: {}".format(mean_error/len(objPoints)))
+
