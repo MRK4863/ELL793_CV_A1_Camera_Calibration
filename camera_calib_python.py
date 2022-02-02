b73a10568da28ffbe420e8a5f056e3b1af8f4abb
MRK4863 <mrkmrk1999@gmail.com>
2022-02-02 13:28:52 +0530

    refined dataset and code

--- camera_calib_python.py
+++ camera_calib_python.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 import glob
  
-chessboard_size = (16,12)
+chessboard_size = (24,17)
 frameSize = (1440, 1080)
 
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_EPS, 30, 0.001)
@@ -14,7 +14,7 @@ objp[:,:2] = np.mgrid[0:chessboard_size[0], 0:chessboard_size[1]].T.reshape(-1,2
 objPoints = []
 imgPoints = []
 
-images = glob.glob("E:\GitHub\CV_Assignment_1\TEST_DATA\*.jpg")
+images = glob.glob("E:\GitHub\CV_Assignment_1\SAMPLE_IMAGES\*.png")
 for image in images:
     print(image)
     img = cv.imread(image)
@@ -44,6 +44,10 @@ print("\nCamera Matrix: \n", cameraMatrix)
 print("\nDistortion parameters: \n", dist)
 print("\nRotation parameters: \n", rvecs)
 print("\nTranslation parameters: \n", tvecs)
+
+
+print('Normalized camera matrix:', cameraMatrix/cameraMatrix[2][2])
+
 
 #ERROR
 
